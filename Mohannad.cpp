417dad14258eb704c6293d5efc5841a8e0df6724
Mohannad Read <mohannadreda123@gmail.com>
2026-04-28 17:05:27 +0300

    developments

--- Mohannad.cpp
+++ Mohannad.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
+#include <iostream>
 #include <cstdlib>
-#include <ctime> 
+#include <ctime>
 using namespace std;
 void showpage()
 {
@@ -11,7 +11,7 @@ void showpage()
     cout << "Paper vs scissor => Scissor wins.\n\n";
     cout << "Scissor vs Rock => Rock wins.\n\n";
 }
-void check(char& letter)
+void check(char &letter)
 {
     if (letter != 'r' && letter != 'p' && letter != 's')
     {
@@ -34,10 +34,21 @@ char computer()
     char AI_char;
     srand(time(0));
     int number = rand() % 3;
-    if (number == 0) AI_char = 'r';
-    else if (number == 1) AI_char = 'p';
-    else AI_char = 's';
+    if (number == 0)
+        AI_char = 'r';
+    else if (number == 1)
+        AI_char = 'p';
+    else
+        AI_char = 's';
     return AI_char;
+}
+int AI_score = 0;
+int player_score = 0;
+void score(int AI_score, int player_score, string name)
+{
+    cout << "\t\t\t\t\t\tTotal Score\n\n";
+    cout << "\t\t\t\t\t\tAI Score => " << AI_score << "\n\n";
+    cout << "\t\t\t\t\t\t" << name << " Score => " << player_score << "\n\n";
 }
 void winner(char letter, string name)
 {
@@ -47,33 +58,51 @@ void winner(char letter, string name)
         system("cls");
         if (AI_char == 'r' && letter == 'p')
         {
+            player_score++;
+            cout << "Your choice : " << letter << "\n\n";
             cout << "AI choice : " << AI_char << "\n\n";
-            cout << name << " Wins.";
+            cout << name << " Wins.\n\n";
+            score(AI_score, player_score, name);
         }
         else if (AI_char == 'p' && letter == 'r')
         {
+            AI_score++;
+            cout << "Your choice : " << letter << "\n\n";
             cout << "AI choice : " << AI_char << "\n\n";
-            cout << "AI Wins.";
+            cout << "AI Wins.\n\n";
+            score(AI_score, player_score, name);
         }
         else if (AI_char == 'r' && letter == 's')
         {
+            AI_score++;
+            cout << "Your choice : " << letter << "\n\n";
             cout << "AI choice : " << AI_char << "\n\n";
-            cout << "AI Wins.";
+            cout << "AI Wins.\n\n";
+            score(AI_score, player_score, name);
         }
         else if (AI_char == 's' && letter == 'r')
         {
+            player_score++;
+            cout << "Your choice : " << letter << "\n\n";
             cout << "AI choice : " << AI_char << "\n\n";
-            cout << name << " Wins.";
+            cout << name << " Wins.\n\n";
+            score(AI_score, player_score, name);
         }
         else if (AI_char == 'p' && letter == 's')
         {
+            player_score++;
+            cout << "Your choice : " << letter << "\n\n";
             cout << "AI choice : " << AI_char << "\n\n";
-            cout << name << " Wins.";
+            cout << name << " Wins.\n\n";
+            score(AI_score, player_score, name);
         }
         else if (AI_char == 's' && letter == 'p')
         {
+            AI_score++;
+            cout << "Your choice : " << letter << "\n\n";
             cout << "AI choice : " << AI_char << "\n\n";
-            cout << "AI Wins.";
+            cout << "AI Wins.\n\n";
+            score(AI_score, player_score, name);
         }
     }
     else
@@ -106,6 +135,23 @@ void game(string name)
     check(letter);
     winner(letter, name);
 }
+bool checker = true;
+void playagain(string name)
+{
+    string again = "yes";
+    cout << "Do you want to play again?\n";
+    cin >> again;
+    if (again == "yes" || again == "Yes" || again == "YES")
+    {
+        game(name);
+    }
+    else
+    {
+        checker = false;
+        system("cls");
+        showpage();
+    }
+}
 int main()
 {
     // To increase (cin,cout) compilation process
@@ -118,5 +164,9 @@ int main()
     cout << "Enter Player name : ";
     cin >> name;
     game(name);
+    while (checker)
+    {
+        playagain(name);
+    }
     return 0;
 }
